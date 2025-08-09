b0185768d414cea8abf451dcd8c4d0bb6d2cf745
困住你的不是别人 <116521229+griffith-xx@users.noreply.github.com>
2025-08-09 11:10:10 +0700

    Refactor UserInteraction and UserPreference models to define fillable attributes for mass assignment

--- app/Models/UserInteraction.php
+++ app/Models/UserInteraction.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserInteraction extends Model
 {
-    //
+    // Fillable attributes for mass assignment
+    protected $fillable = [
+        'user_id',
+        'attraction_id',
+        'interaction_type',
+        'target_id',
+        'rating',
+        'comment',
+        'metadata',
+        'interaction_date'
+    ];
 }
--- app/Models/UserPreference.php
+++ app/Models/UserPreference.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserPreference extends Model
 {
-    //
+    // Fillable attributes for mass assignment
+    protected $fillable = [
+        'user_id',
+        'preference_type',
+        'preference_value',
+        'weight'
+    ];
 }
