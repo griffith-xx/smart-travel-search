bc0cc7608ef6fefa245db023f361287378a8fb55
困住你的不是别人 <116521229+griffith-xx@users.noreply.github.com>
2025-08-12 15:10:21 +0700

    feat: integrate Tailwind CSS forms, typography, and postcss; update Welcome page layout
    - Added dependencies for Tailwind CSS forms, postcss, and typography in package.json.
    - Removed unused dark mode custom variant from app.css.
    - Deleted unused useLayout.js composable.
    - Refactored Welcome.vue to enhance layout and functionality, including dynamic image handling and improved navigation.
    - Introduced a new RootLayout.vue component for consistent page structure.
    - Updated app.js to define a custom theme preset for PrimeVue.

--- resources/js/Composables/useLayout.js
+++
@@ -1,8 +0,0 @@
-import { updatePrimaryPalette, updateSurfacePalette } from "@primeuix/themes";
-import { computed, ref } from "vue";
-
-const appState = ref({
-    primary: "emerald",
-    surface: null,
-    darkMode: false
-});
---
+++ resources/js/Layouts/RootLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head :title="title" />
+    <slot></slot>
+</template>
