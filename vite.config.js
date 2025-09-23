f015329cceaacea678f7b309025cd69ab6b004a8
困住你的不是别人 <116521229+griffith-xx@users.noreply.github.com>
2025-09-23 21:50:33 +0700

    C

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import Components from "unplugin-vue-components/vite";
+import { PrimeVueResolver } from "@primevue/auto-import-resolver";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
@@ -16,6 +19,10 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }), 
+        Components({
+            resolvers: [PrimeVueResolver()],
         }),
+        tailwindcss(),
     ],
 });
