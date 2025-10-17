685b3064ab553268fcb5dbfb60adc050e2547b5e
困住你的不是别人 <116521229+griffith-xx@users.noreply.github.com>
2025-10-17 19:40:15 +0700

    c

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,23 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
-use App\Models\Destination;
-use App\Models\Province;
 use Inertia\Inertia;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
-        $provinces = Province::withCount('destinations')->get();
-        $categories = Category::withCount('destinations')->get();
-        $destinations = Destination::with('province')->get();
-
-        return Inertia::render('Welcome', [
-            'provinces' => $provinces,
-            'categories' => $categories,
-            'destinations' => $destinations,
-        ]);
+        return Inertia::render('Welcome');
     }
 }
